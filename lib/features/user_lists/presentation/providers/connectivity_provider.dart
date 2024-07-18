fffbdaf0bfe3a7830ed33b79af08afbb8743ce88
trishaagaba <90703139+trishaagaba@users.noreply.github.com>
2024-07-18 16:54:23 +0300

    Recent changes

---
+++ lib/features/user_lists/presentation/providers/connectivity_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ConnectivityProvider extends ChangeNotifier{
+  ConnectivityResult _connectivityResult = ConnectivityResult.none;
+  bool _isConnected = false;
+
+  ConnectivityProvider(){
+    _checkConnectivity();
+    Connectivity().onConnectivityChanged.listen((ConnectivityResult result){
+      _updateConnectionStatus(result);
+    });
+  }
+
+  bool get isConnected => _isConnected;
+
+  Future<void> _checkConnectivity() async {
+    final ConnectivityResult result = await Connectivity().checkConnectivity();
+    _updateConnectionStatus(result);
+  }
+
+  void _updateConnectionStatus(ConnectivityResult result) {
+    _connectivityResult = result;
+    _isConnected = _connectivityResult != ConnectivityResult.none;
+    notifyListeners();
+  }
+
+}
---
+++ lib/features/user_lists/presentation/widget/filter_widget.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class FilterWidget extends StatefulWidget {
+  final Function(String, String, int, int) onFilterChanged;
+
+  FilterWidget({required this.onFilterChanged});
+
+  @override
+  _FilterWidgetState createState() => _FilterWidgetState();
+}
+  class _FilterWidgetState extends State<FilterWidget> {
+    final TextEditingController _nameController = TextEditingController();
+    final TextEditingController _typeController = TextEditingController();
+    int _followers = 0;
+    int _following = 0;
+
+    // final TextEditingController _followersController = TextEditingController();
+    // final TextEditingController _followingController = TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText: 'By name',
+            ),
+          ),
+          TextField(
+            controller: _typeController,
+            decoration: InputDecoration(
+              labelText: 'By type',
+            ),
+          ),
+          SizedBox(height: 16.0),
+          Column(
+          children: [
+          Row(
+            children: [
+              Text("Followers: {$_followers}"),
+      Expanded(
+      child: Slider(
+                  value: _followers.toDouble(),
+                  min: 0,
+                  max: 1000,
+                  divisions: 1000,
+                  label: _followers.toString(),
+                  onChanged: (value) {
+                    setState(() {
+                      _followers = value.toInt();
+                    });
+                  }),)
+
+                   ],),
+                   Row(
+              children: [
+              Text("Following: {$_following}"),
+      Expanded(
+      child: Slider(
+                  value: _following.toDouble(),
+                  min: 0,
+                  max: 1000,
+                  divisions: 1000,
+                  label: _following.toString(),
+                  onChanged: (value) {
+                    setState(() {
+                      _following = value.toInt();
+                    });
+                  }))
+      ]
+                  )
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              widget.onFilterChanged(
+                  _nameController.text,
+                  _typeController.text,
+                  _followers,
+                  _following
+              );
+              Navigator.of(context).pop();
+            },
+            child: Text('Apply filters'),
+          ),
+        ],
+      );
+    }
+  }
+
+
+
